09907c8483e24a9541b8ab5c36d522768d1e16b0
Philipp Philippov <feelstarsh2@yandex.ru>
2023-04-15 13:15:42 +0300

    Упрощенное решение задания №1

--- Laba_6/ex1.c
+++ Laba_6/ex1.c
@@ -2,38 +2,28 @@
 #include <stdlib.h>
 // #include <string.h>
 
-int maxLength(char arr[], char sym);
+int maxLength(char sym);
 
+char arrSym[] = {'h', 'h','e','l','l','l','o', 'o','l','o', 'h', '\n'};
 
 int main() {
-    char arrSym[] = {'h','e','l','l','l','o', 'o','l','o','\n'};
     char sym;
 
     printf("Enter a symbol: \n");
     scanf(" %c", &sym);
-    printf("%d", maxLength(arrSym, sym));
+    printf("%d", maxLength(sym));
 
     return 0;
 }
 
-int maxLength(char arr[], char sym) {
+int maxLength(char sym) {
     
     int counter = 0;
     int maxCounter = 0;
     int i = 0;
-    int arrLength = 8;
-    char* ptrArr;
 
-    ptrArr = (char*)malloc(arrLength * sizeof(char));
-	
-    for (i = 0; i < arrLength; i++) {
-		ptrArr[i] = arr[i];
-	}
-    
-    i = 0;
-
-    while  (i < arrLength) {
-            if (arr[i] == sym) {
+    while  (i < sizeof(arrSym)) {
+            if (arrSym[i] == sym) {
                 counter += 1;
                 if (counter > maxCounter) {
                     maxCounter = counter;
@@ -43,6 +33,5 @@ int maxLength(char arr[], char sym) {
             }
             i += 1;
         }
-    
     return maxCounter;
 }
